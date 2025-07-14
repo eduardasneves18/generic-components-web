bd0919b8c821c7ca1651afa822a31a6622440e27
Eduarda <eduardasneves18@gmail.com>
2025-07-13 21:56:15 -0300

    TC5

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,28 +1,36 @@
 import React from 'react';
-// import Link from '../link/Link';
 import "./Menu.css";
 
-const MenuItens: React.FC = () => {
-    return(
-        <ul className= "itens-menu-lateral">
-            <li><a className= "item" href='/'>Página inicial</a></li>
-            <hr />
-            <li><a className= "item" href='/statement'>Extrato Detalhado</a></li>
-            <hr />
-            <li><a className= "item" href='/'>Cartões</a></li>
-            <hr />
-            <li><a className= "item" href='/transaction'>Nova Transação</a></li>
-            <hr />
-            <li><a className= "item" href='/'>Todos os serviços</a></li>
-        </ul>  
-    );
-}
-const Menu:React.FC = () => {
-    return(
-        <div className="lateral-diminuido">
-            <MenuItens></MenuItens> 
-        </div>
-    );
-}
+type MenuItem = {
+  label: string;
+  path: string;
+};
+
+type MenuProps = {
+  items: MenuItem[];
+};
+
+const MenuItens: React.FC<MenuProps> = ({ items }) => {
+  return (
+    <ul className="itens-menu-lateral">
+      {items.map((item, index) => (
+        <React.Fragment key={index}>
+          <li>
+            <a className="item" href={item.path}>{item.label}</a>
+          </li>
+          {index < items.length - 1 && <hr />}
+        </React.Fragment>
+      ))}
+    </ul>
+  );
+};
+
+const Menu: React.FC<MenuProps> = ({ items }) => {
+  return (
+    <div className="lateral-diminuido">
+      <MenuItens items={items} />
+    </div>
+  );
+};
 
 export default Menu;
