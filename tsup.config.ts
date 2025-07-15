6ce75e5a4dabf11c7c7054a8f3965b3cc6d28875
Eduarda <eduardasneves18@gmail.com>
2025-07-14 23:57:00 -0300

    14/07/2025

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,17 +1,28 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsup'
+import { globSync } from 'glob'
+import path from 'path'
 
 export default defineConfig({
-  entry: ['src/index.ts'],  
-  format: ['esm', 'cjs'],  
-  dts: true,               
-  minify: true,            
-  sourcemap: true,         
-  clean: true,              
-  splitting: false,         
-  shims: false,            
-  bundle: true,            
-  external: ['react', 'react-dom'],  
-  loader: {
-    '.css': 'css',       
+  entry: [
+    'src/index.ts',
+    ...globSync('src/**/*.css')
+  ],
+  format: ['esm', 'cjs'],
+  dts: {
+    entry: Object.fromEntries(
+      globSync('src/**/*.{ts,tsx}').map(file => [
+        path.relative('src', file).replace(/\.(ts|tsx)$/, ''),
+        path.resolve(file)
+      ])
+    )
   },
-});
+  minify: true,
+  sourcemap: true,
+  clean: true,
+  splitting: false,
+  bundle: true,
+  external: ['react', 'react-dom'],
+  loader: {
+    '.css': 'copy'
+  }
+})
