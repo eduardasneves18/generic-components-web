a5761b277670fd524484c9bd823ca8326c4ee08f
Eduarda <eduardasneves18@gmail.com>
2025-07-16 20:33:14 -0300

    16/07/2025

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,3 @@
-import './style/components.global.css';
 import { AttachmentField, DateField, EmailField, NumberField, TextField, LookupField, PasswordField, Header, Menu, Modal, Dashboard, Button, LoginButton, FilterIconButton, NextLinkWrapper } from './components';
 export * from './components';
 export { AttachmentField, DateField, EmailField, NumberField, TextField, LookupField, PasswordField, Header, Menu, Modal, Dashboard, Button, LoginButton, FilterIconButton, NextLinkWrapper, };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import './style/components.global.css';
+// import './style/components.global.css';
 
 import {
   AttachmentField,
